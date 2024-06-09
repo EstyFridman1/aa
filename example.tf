41a610dcaa231ecc0d5c77919fcf5a0e541c2aaf
tzippykovalsky <esty62133@gmail.com>
2024-06-09 12:13:02 +0300

    tow commit

--- example.tf
+++ example.tf
@@ -2,8 +2,9 @@
 resource "aws_athena_workgroup" "analytics1" {
   name = "analytics1"
 
-  # no tags
-
+  tags = merge(module.mylabel.tags, {
+    Group = "DataEngineering"
+  })
   configuration {
     enforce_workgroup_configuration = true
   }
